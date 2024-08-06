37b9bf86280ebdb24dc866d0af80498810a6d5c3
Ding-Han Sean Yan <sean@dhsy.me>
2024-08-06 20:14:33 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Sean's Nix Darwin Setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+    	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }: {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#
+    darwinConfigurations."Ding-Hans-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      specialArgs =  { inherit inputs; };
+      modules = [ 
+	      ./configuration.nix 
+	      nix-homebrew.darwinModules.nix-homebrew {
+		      nix-homebrew = {
+				# Install Homebrew under the default prefix
+			      enable = true;
+
+				# Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+			      enableRosetta = true;
+
+				# User owning the Homebrew prefix
+			      user = "tzen";
+
+				# Optional: Declarative tap management
+			      taps = {
+				      "homebrew/homebrew-core" = homebrew-core;
+				      "homebrew/homebrew-cask" = homebrew-cask;
+			      };
+
+# Optional: Enable fully-declarative tap management
+#
+# With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+			      mutableTaps = false;
+			      
+		      };
+	      }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Ding-Hans-MacBook-Pro".pkgs;
+
+    homeConfigurations.tzen = home-manager.lib.homeManagerConfiguration {
+    	modules = [ ./home ];
+    };
+  };
+}
