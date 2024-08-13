4a3f27149a4a156cce73a0c7288b46341404229d
Ding-Han Sean Yan <sean@dhsy.me>
2024-08-14 00:34:58 +1000

    settings: autohide dock

--- hosts/macbookpro16/system-settings.nix
+++ hosts/macbookpro16/system-settings.nix
@@ -1,11 +1,17 @@
 { ... }: 
 {
-    system.defaults.NSGlobalDomain = {
-        # AppleKeyboardUIMode = 3;
-        InitialKeyRepeat = 5;
-        KeyRepeat = 1;
-        ApplePressAndHoldEnabled = false;
+    system.defaults = {
+        NSGlobalDomain = {
+            # AppleKeyboardUIMode = 3;
+            InitialKeyRepeat = 5;
+            KeyRepeat = 1;
+            ApplePressAndHoldEnabled = false;
+        };
+        dock.autohide = true;
     };
+
+
+
 
     system.keyboard = {
         enableKeyMapping = true;
