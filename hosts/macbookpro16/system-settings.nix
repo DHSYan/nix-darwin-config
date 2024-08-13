191414907803b54daea898a0cbfa54060b18c853
Ding-Han Sean Yan <sean@dhsy.me>
2024-08-14 00:14:56 +1000

    settings: keyboard settings

--- hosts/macbookpro16/system-settings.nix
+++ hosts/macbookpro16/system-settings.nix
@@ -1,11 +1,12 @@
 { ... }: 
 {
     system.defaults.NSGlobalDomain = {
-        InitialKeyRepeat = 1;
-        KeyRepeat = 5;
+        InitialKeyRepeat = -1;
+        KeyRepeat = 10;
     };
 
     system.keyboard = {
+        enableKeyMapping = true;
         remapCapsLockToEscape = true;
     };
 }
